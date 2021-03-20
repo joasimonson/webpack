59c79e1a011bb084301170a966fb6c6aa2850ff1
joasimonson <joasimonson@hotmail.com>
2021-03-20 15:43:14 -0300

    configuração para minificar css

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 let plugins = [];
 
@@ -8,6 +9,16 @@ plugins.push(new extractTextPlugin('styles.css'));
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new babiliPlugin());
+
+  plugins.push(new optimizeCSSAssetsPlugin({
+    cssProcessor: require('cssnano'),
+    cssProcessorOptions: {
+      discardComments: {
+        removeAll: true
+      }
+    },
+    canPrint: true
+  }))
 }
 
 module.exports = {
