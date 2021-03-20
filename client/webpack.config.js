3e6cf2abcef326900493a620989c1c1b731fdd68
joasimonson <joasimonson@hotmail.com>
2021-03-20 15:35:31 -0300

    tratamento para evitar fouc flash of unstyled component

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new extractTextPlugin('styles.css'));
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new babiliPlugin());
@@ -25,7 +28,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        use: extractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
