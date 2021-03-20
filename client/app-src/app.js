9bf4439e2b55c144ed43fd681dcaf8bb966eead2
joasimonson <joasimonson@hotmail.com>
2021-03-20 15:26:31 -0300

    importação de css de pacote e interno como módulo para otimização

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,8 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../css/default.css';
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const babiliPlugin = require('babili-webpack-plugin');
 let plugins = [];
 
 if (process.env.NODE_ENV === 'production') {
-    plugins.push(new babiliPlugin());
+  plugins.push(new babiliPlugin());
 }
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    publicPath: 'dist',
   },
   module: {
     rules: [
@@ -23,7 +23,27 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
     ],
   },
-  plugins
+  plugins,
 };
