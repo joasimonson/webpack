50d6625b020a33475b3d68aee4d62b9634dc1882
joasimonson <joasimonson@hotmail.com>
2021-03-20 16:14:43 -0300

    otimização para carga de módulos

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,6 +16,7 @@ plugins.push(
 );
 
 if (process.env.NODE_ENV === 'production') {
+  plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
   plugins.push(new babiliPlugin());
 
   plugins.push(
